e4a67f400573c7f96783e8324fff3ff77498e775
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-04-10 19:33:49 +0000

    Proyecto terminado

--- .learn/resets/11-homework/app.js
+++ .learn/resets/11-homework/app.js
@@ -1,33 +1,35 @@
+// Función de suma de ejemplo
 const sum = function(a, b) {
     return a + b;
 }
 
-// Euro to Dollar
+// Convertir de euro a dólar
 const fromEuroToDollar = function(valueInEuro) {
     let valueInDollar = valueInEuro * 1.07;
     return valueInDollar;
 }
 
-// Euro to Yen
+// Convertir de euro a yen
 const fromEuroToYen = function(valueInEuro) {
     let valueInYen = valueInEuro * 156.5;
     return valueInYen;
 }
 
-// Yen to Dollar
+// Convertir de yen a dólar
 const fromYenToDollar = function(valueInYen) {
     let valueInEuro = valueInYen / 156.5;
     let valueInDollar = valueInEuro * 1.07;
     return valueInDollar;
 }
 
-// Yen to Pound
+// Convertir de yen a libra esterlina
 const fromYenToPound = function(valueInYen) {
     let valueInEuro = valueInYen / 156.5;
     let valueInPound = valueInEuro * 0.87;
     return valueInPound;
 }
 
+// Exportar todas las funciones
 module.exports = {
     sum,
     fromEuroToDollar,
